664884878379a6711327b9b6039d673eca80b168
Landon Turner <lturner@teamworks.com>
2020-08-05 17:19:19 -0400

    close to passing word count

--- applications/word_count/word_count.py
+++ applications/word_count/word_count.py
@@ -1,7 +1,22 @@
+import re
+
 def word_count(s):
     # Your code here
-    # return a dictionary with the key being s and the value being the number of words
-    pass
+    word_dict = {}
+    words = s.split()
+    # add each word as a key
+    ignore = '[.":;,-+=/\|[]{}()*^&]'
+    for word in words:
+        # before this was .isalnum which was clear of special characters but also got rid of the ' in doesn't
+        filtered = ''.join(letter for letter in word if not letter == re.match(letter,ignore))
+        key_word = filtered.lower()
+        print("KEY WORD", key_word)
+        # if the key exist, iterate the value of that key
+        if key_word not in word_dict:
+            word_dict[key_word] = 1
+        else:
+            word_dict[key_word] += 1
+    return word_dict
 
 
 if __name__ == "__main__":
